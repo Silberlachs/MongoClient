32ed2a02cceb11662c329a84456c2c3602d380a6
paw <paw@electronic-minds.de>
2022-05-18 18:01:47 +0200

    Zwischenstand

--- src/main/java/mongoclient/main/DatabaseController.java
+++ src/main/java/mongoclient/main/DatabaseController.java
@@ -3,11 +3,17 @@ package mongoclient.main;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.bson.Document;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -24,83 +30,73 @@ public class DatabaseController implements Initializable {
     private Label db_text;
 
     @FXML
-    private ListView collection_list;
+    private ListView<String> collection_list;
 
     @FXML
-    private TableView collection_items;
+    private TableView<ObservableList<StringProperty>> collection_items;
 
     @FXML
     private Button load_items, back;
 
-    public void initialize() {}
+    public void initialize() {
+    }
 
 
-    public void userDataHack()
-    {
-        if(alreadyCalled)
+    public void userDataHack() {
+        if (alreadyCalled)
             return;
 
         String databaseName = db_text.getScene().getRoot().getUserData().toString();
         database = MongoConnectionGate.getInstance().getDatabase(databaseName);
 
-        for (String name  : database.listCollectionNames()) {
+        for (String name : database.listCollectionNames()) {
             collection_list.getItems().add(name);
         }
 
         alreadyCalled = true;
     }
 
-    public void loadEntries(ActionEvent event){
+    public void loadEntries(ActionEvent event) {
 
-        if(collection_list.getSelectionModel().getSelectedItem() == null)
+        if (collection_list.getSelectionModel().getSelectedItem() == null)
             return;
 
-       String targetCollection = collection_list.getSelectionModel().getSelectedItem().toString();
-       MongoCollection<Document> collection = database.getCollection(targetCollection);
+        String targetCollection = collection_list.getSelectionModel().getSelectedItem().toString();
+        MongoCollection<Document> collection = database.getCollection(targetCollection);
 
-       ArrayList<String> unifiedKeyCollection = new ArrayList<String>();
+        ArrayList<String> unifiedKeyCollection = new ArrayList<String>();
 
-       //extract table column names from data model in database
-       for(Document entries : collection.find()){
-           Set keySet = entries.keySet();
+        //extract table column names from data model in database
+        for (Document entries : collection.find()) {
+            Set keySet = entries.keySet();
 
-           for (Object key : keySet)
-           {
+            for (Object key : keySet) {
+                if (!unifiedKeyCollection.contains(key.toString())) {
+                    unifiedKeyCollection.add(key.toString());
+                }
+            }
+        }
 
-               if(!unifiedKeyCollection.contains(key.toString())){
-                   unifiedKeyCollection.add(key.toString());
-               }
-           }
-       }
-
-        for(String keyNameInTable : unifiedKeyCollection)
-        {
+        for (String keyNameInTable : unifiedKeyCollection) {
+            //TableColumn column = new TableColumn(keyNameInTable);
+           // column.setCellValueFactory(new PropertyValueFactory<>(keyNameInTable));
             collection_items.getColumns().addAll(new TableColumn(keyNameInTable));
         }
 
-/*
-        TableColumn producer = new TableColumn("Hersteller");
-        producer.getColumns().addAll(
-                new TableColumn("Name"),
-                new TableColumn("Preis")
-        );
+        int counter = 0;
+        for (Document element : collection.find()) {
 
-        collection_items.getColumns().addAll(
-                new TableColumn("Name"),
-                new TableColumn("Kategorie"),
-                new TableColumn("Preis"),
-                producer,
-                new TableColumn("Tags")
-        );
-*/
+            ObservableList<StringProperty> tableRow = FXCollections.observableArrayList();
 
-       for (Document element: collection.find()){
+            for (Object valueString : element.values()) {
+                tableRow.add(counter,new SimpleStringProperty(element.toString()));
+                counter++;
 
-            for (Object valueString : element.values()){
-
-                collection_items.getItems().addAll(new TableColumn(valueString.toString()));
             }
-       }
+            collection_items.getItems().add(tableRow);
+
+        }
+
     }
 
     public void mainMenu(ActionEvent event) throws IOException {
@@ -109,5 +105,6 @@ public class DatabaseController implements Initializable {
     }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {}
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
